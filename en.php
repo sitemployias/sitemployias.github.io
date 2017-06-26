a3b941810979e11f7d139f3b63d7c4d2200f25e0
sitemployias <sitemployias@gmail.com>
2017-06-26 18:25:35 +0700

    Create en.php

---
+++ en.php
@@ -0,0 +1,13 @@
+<?php
+	$strTitle = "Welcome to My Web Site";
+	$strHome = "Home";
+	$strService = "Service";
+	$strAbout = "About Us";
+	$strContact = "Contact Us";
+
+
+	//** สมมุติค่ามาจาก Database ที่ได้จากการ Query ***//
+	$result["DETAIL_EN"] = "This is content English";
+
+	$strDetail = $result["DETAIL_EN"];
+?>

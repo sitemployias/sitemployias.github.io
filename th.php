f3d6666004257c239231f3ee3a069f1f57c4f4df
sitemployias <sitemployias@gmail.com>
2017-06-26 18:25:04 +0700

    Create th.php

---
+++ th.php
@@ -0,0 +1,12 @@
+<?php
+	$strTitle = "ยินดีต้อนรับเข้าสู่เว็บไซต์ของฉัน";
+	$strHome = "หน้าแรก";
+	$strService = "บริการของเรา";
+	$strAbout = "เกี่ยวกับเรา";
+	$strContact = "ติดต่อเรา";
+
+	//** สมมุติค่ามาจาก Database ที่ได้จากการ Query ***//
+	$result["DETAIL_TH"] = "นี้คือเนื้อหาภาษาไทย";
+
+	$strDetail = $result["DETAIL_TH"];
+?>
